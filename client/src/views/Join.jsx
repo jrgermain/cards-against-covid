8def7886f37b365e36c54717c06fd30e00c56e78
jrgermain <37086292+jrgermain@users.noreply.github.com>
2020-11-08 21:46:38 -0500

    Show error when game code is invalid

--- client/src/views/Join.jsx
+++ client/src/views/Join.jsx
@@ -3,9 +3,11 @@ import Button from '../components/Button';
 import './Join.css';
 import { useHistory} from 'react-router-dom';
 import Ajax from '../lib/ajax';
+import { useState } from 'react';
 
 function Join() {
     const history = useHistory();
+    const [isError, setError] = useState(false);
 
     async function joinGame() {
         try {
@@ -13,8 +15,13 @@ function Join() {
             await Ajax.getJson("./api/findGame?code=" + code, { cache: false });
             history.push("/waiting/" + code);
         } catch (e) {
-            console.log(e)
+            setError(true);
         }
+    }
+
+    let inputClass = "big-text uppercase";
+    if (isError) {
+        inputClass += " error";
     }
 
     return (
@@ -22,7 +29,8 @@ function Join() {
             <h1>Join a game</h1>
             <div className="game-code-entry">
                 <label htmlFor="game-code">Enter game code: </label>
-                <input id="game-code" type="text" placeholder="Game Code" className="big-text uppercase" maxLength="4"></input>
+                <input id="game-code" type="text" placeholder="Game Code" className={inputClass} maxLength="4"></input>
+                {isError && <span className="error-text">The game you specified doesn't exist or isn't accepting players.</span>}
             </div>
             <Button onClick={joinGame}>Join</Button>
 
