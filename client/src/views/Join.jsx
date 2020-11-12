09f9a03ea431eadec835661a45a7370069c396ee
jrgermain <37086292+jrgermain@users.noreply.github.com>
2020-11-11 21:32:57 -0500

    Read/write from input fields in a more React-y way

--- client/src/views/Join.jsx
+++ client/src/views/Join.jsx
@@ -9,16 +9,10 @@ import { useEffect } from 'react';
 function Join() {
     const history = useHistory();
     const [errorCode, setErrorCode] = useState(0);
-
-    // On the first render, pre-fill the name field with the last-used name
-    useEffect(() => {
-        document.getElementById("player-name").value = localStorage.getItem("player-name");
-    }, []);
+    const [name, setName] = useState(localStorage.getItem("player-name"));
+    const [gameCode, setGameCode] = useState("");
 
     async function joinGame() {
-        const code = document.getElementById("game-code").value;
-        const name = document.getElementById("player-name").value;
-
         // Validate name
         if (!name) {
             return setErrorCode(1);
@@ -26,14 +20,14 @@ function Join() {
 
         // Validate game code
         try {
-            await Ajax.postJson("./api/joinGame", JSON.stringify({ code, name }));
+            await Ajax.postJson("./api/joinGame", JSON.stringify({ code: gameCode, name }));
         } catch (e) {
             return setErrorCode(2);
         }
 
         // Proceed to the next page
         localStorage.setItem("player-name", name);
-        history.push("/waiting/" + code);
+        history.push("/waiting/" + gameCode);
     }
 
     function handleKeyPress(event) {
@@ -48,12 +42,12 @@ function Join() {
             <section className="user-info">
                 <div>
                     <label htmlFor="player-name">Enter your name: </label>
-                    <input id="player-name" type="text" placeholder="Your name" className={"big-text"} data-error={errorCode === 1} onKeyPress={handleKeyPress}></input>
+                    <input id="player-name" type="text" placeholder="Your name" className={"big-text"} data-error={errorCode === 1} onKeyPress={handleKeyPress} value={name} onChange={e => setName(e.target.value)}></input>
                     <span className="error-text">Please enter a name.</span>
                 </div>
                 <div>
                     <label htmlFor="game-code">Enter game code: </label>
-                    <input id="game-code" type="text" placeholder="Game Code" className={"big-text uppercase"} data-error={errorCode === 2} maxLength="4" onKeyPress={handleKeyPress}></input>
+                    <input id="game-code" type="text" placeholder="Game Code" className={"big-text uppercase"} data-error={errorCode === 2} maxLength="4" onKeyPress={handleKeyPress} value={gameCode} onChange={e => setGameCode(e.target.value)}></input>
                     <span className="error-text">The game you specified doesn't exist or isn't accepting players.</span>
                 </div>
             </section>
