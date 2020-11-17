a9f5fc66226eb15a228f1eeb901676b9d6b88c4e
Amanda Etienne <ajetienne@qu.edu>
2020-11-17 18:51:28 -0500

    packs implemented on choose deck screen

--- client/src/views/ChooseDeck.jsx
+++ client/src/views/ChooseDeck.jsx
@@ -40,21 +40,42 @@ function ChooseDeck() {
             const gameDeck = Ajax.post("./api/childDeck");
             console.log(gameDeck);
         }
+        if (event.target.value == 2) {
+            const gameDeck = Ajax.post("./api/packDisney");
+            console.log(gameDeck);
+        }
+        if (event.target.value == 3) {
+            const gameDeck = Ajax.post("./api/packHarryPotter");
+            console.log(gameDeck);
+        }
+        if (event.target.value == 4) {
+            const gameDeck = Ajax.post("./api/packVine");
+            console.log(gameDeck);
+        }
     }
 
     return (
         <div className="view" id="choose-deck">
-            <h1>Choose a deck</h1>
+            <h1>Choose a Deck</h1>            
 
             <label htmlFor="player-name">Enter your name: </label>
             <input id="player-name" type="text" placeholder="Your name" className={"big-text"} data-error={hasSubmitted && !name} value={name} onChange={e => setName(e.target.value)}></input>
             <span className="error-text">Please enter a name.</span>
 
+
             <select class="select-css" onChange={handleChange}>
-                <option class="select-items" value="0"  > Adult (18+)</option>
-                <option class="select-items" value="1"  >Child</option>
+                <option> Choose a Deck</option>
+                <option value="0"  > Adult (18+)</option>
+                <option value="1"  >Child</option>
             </select>
 
+            <h1 class="header-css">Choose an Expansion Pack</h1>
+            <select class="select-css" onChange={handleChange}>
+                <option> Choose an Expansion Pack (optional)</option>
+                <option value="2"  > Disney... Plus? (18+)</option>
+                <option value="3"  >Hogwarts After Hours (18+)</option>
+                <option value="4"  >Rest in Peace: Vine References</option>
+            </select>
 
             <Button onClick={initGame}>Continue</Button>
         </div>
