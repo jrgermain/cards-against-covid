bd8e7052b03e378f84d61363af763ff28a609ecc
jrgermain <37086292+jrgermain@users.noreply.github.com>
2020-12-22 19:37:19 -0500

    Fix issue where extra history entries were added

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Button.css';
 
-function Button({ link = "#", onClick, children }) {
-    return (
-        <Link to={link} className="button-wrapper">
-            <button onClick={onClick}>{children}</button>
-        </Link>
-    );
+function Button({ link, onClick, children }) {
+    if (link) {
+        return (
+            <Link to={link} className="button-wrapper">
+                <button onClick={onClick}>{children}</button>
+            </Link>
+        )
+    } else {
+        return (
+            <span className="button-wrapper">
+                <button onClick={onClick}>{children}</button>
+            </span>
+        )
+    }
 }
 
 export default Button;
