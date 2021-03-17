4d791ead87822a1a8a40b2809d0c44b6e73481c7
jrgermain <37086292+jrgermain@users.noreply.github.com>
2021-03-17 19:13:26 -0400

    Make error handling and comments consistent with other views

--- client/src/views/Expansions.jsx
+++ client/src/views/Expansions.jsx
@@ -5,16 +5,19 @@ import DeckMaker from '../components/DeckMaker';
 import TextBox from '../components/TextBox';
 import Ajax from '../lib/ajax';
 import './Expansions.css';
-import { showSuccess } from '../lib/message';
+import { showSuccess, showError } from '../lib/message';
 
 function Expansions() {
     const history = useHistory();
+    
     const [name, setName] = useState("Untitled Expansion Pack");
     const [prompts, setPrompts] = useState([]);
     const [responses, setResponses] = useState([]);
 
+    // Perform basic validations, then send to the server and process response
     async function submitPack() {
         if (!name) {
+            // The pack needs a name. This will display an error with no further action required.
             return;
         }
 
@@ -22,29 +25,34 @@ function Expansions() {
         const filteredPrompts = prompts.filter(x => x);
         const filteredResponses = responses.filter(x => x);
 
+        // Validate pack size
         if (filteredPrompts.length === 0 && filteredResponses.length === 0) {
-            window.alert("Please add at least one non-empty card");
+            showError("Please add at least one non-empty card");
             return;
         }  
 
-        if (filteredPrompts.length < 7 && filteredResponses.length < 21) {
-            window.alert("Please add at least 7 prompt and 21 response cards");
+        if (filteredPrompts.length < 7 || filteredResponses.length < 21) {
+            showError("Please add at least 7 prompt and 21 response cards");
             return;
         }  
 
         const pack = { name, prompts: filteredPrompts, responses: filteredResponses }
         try {
             await Ajax.postJSON("/api/expansionPack", JSON.stringify(pack));
-            showSuccess("Successfully saved expansion pack: " + name);
-            history.goBack();
         } catch (e) {
+            // An error response was received. If it was a known error, display it. Otherwise, log it and display a generic message.
             if (e === "Bad Request") {
-                window.alert("A pack with this name already exists. Please choose a new name.");
+                showError("A pack with this name already exists. Please choose a new name.");
             } else {
-                window.alert("An unexpected error occurred.");
+                showError("An unexpected error occurred.");
                 console.error("Error saving expansion pack: ", e);
             }
+            return;
         }
+
+        // We got a success response, so the pack must have been saved. Bring the user back to the previous page.
+        showSuccess("Successfully saved expansion pack: " + name);
+        history.goBack();
     }
 
     const handleNameChange = e => setName(e.target.value);
@@ -68,7 +76,7 @@ function Expansions() {
             </div>
             <Button onClick={submitPack}>Save</Button>
         </div>
-    )
+    );
 }
 
 export default Expansions;
