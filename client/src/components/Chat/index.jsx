a1086cca661e710347d7328073997f3c5b51d83f
jrgermain <37086292+jrgermain@users.noreply.github.com>
2021-05-18 22:45:23 -0400

    Display the number of unread chat messages

--- client/src/components/Chat/index.jsx
+++ client/src/components/Chat/index.jsx
@@ -7,12 +7,19 @@ import { useSelector } from 'react-redux';
 function Chat({ gameCode, name }) {
     const messages = useSelector(state => state.messages);
 
+    const [numRead, setNumRead] = useState(0);
     const [collapsed, setCollapsed] = useState(true);
-    const toggle = () => setCollapsed(!collapsed);
+    const toggle = () => {
+        setNumRead(messages.length);
+        setCollapsed(!collapsed);
+    }
+
+    const hasUnread = collapsed && numRead < messages.length;
 
     return (
         <aside className="panel chat" data-collapsed={collapsed}>
             <button className="panel-toggle" onClick={toggle} aria-label={collapsed ? "Open chat" : "Dismiss chat"}>Toggle Chat</button>
+            {hasUnread && <span className="unread-count">{messages.length - numRead}</span>}
             <div className="chat-messages" aria-live="polite">
                 {messages.map(ChatMessage)}
             </div>
